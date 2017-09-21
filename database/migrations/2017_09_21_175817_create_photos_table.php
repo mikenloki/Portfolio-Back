d153de806a33543558199c7edcf1de9e6453322f
mikenloki <mikenloki@aol.com>
2017-09-21 16:13:45 -0400

    Some Migrations completed on Back-end.

---
+++ database/migrations/2017_09_21_175817_create_photos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('photos', function (Blueprint $table) {
+          $table->string('title')->index();
+          $table->string('photo_URL')->index();
+          $table->timestamp('created_at')->nullable();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
